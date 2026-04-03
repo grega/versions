7168a8229f01fa7cb596a7ba2e661fcdb6a6b2aa
Greg Annandale <greg@gregannandale.com>
2026-04-03 23:00:53 +0100

    Add api/packages GET endpoint

---
+++ src/routes/api/packages/+server.ts
@@ -0,0 +1,9 @@
+import { json } from '@sveltejs/kit';
+import { loadPackages } from '$lib/config.js';
+import { fetchAllPackages } from '$lib/fetchers/index.js';
+
+export async function GET() {
+	const configs = loadPackages();
+	const packages = await fetchAllPackages(configs);
+	return json(packages);
+}
